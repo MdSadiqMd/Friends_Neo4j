5427d8cadf6ca06f40aac9ec04a007ff56c2718c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-09 11:59:07 +0530

    Feat: Added db driver

---
+++ src/db/config.ts
@@ -0,0 +1,25 @@
+import neo4j, { Driver, Session, Result } from 'neo4j-driver';
+import config from 'config';
+
+const uri: string = config.get<string>('dbHost');
+const user: string = config.get<string>('dbUser');
+const password: string = config.get<string>('dbPass');
+
+const driver: Driver = neo4j.driver(uri, neo4j.auth.basic(user, password), {
+    maxConnectionLifetime: 3 * 60 * 60 * 1000,
+    maxConnectionPoolSize: 50,
+    connectionAcquisitionTimeout: 2 * 60 * 1000,
+    disableLosslessIntegers: true,
+});
+
+export async function executeCypherQuery(statement: string, params: Record<string, any> = {}): Promise<Result> {
+    const session: Session = driver.session();
+    try {
+        const result = await session.run(statement, params);
+        return result;
+    } catch (error) {
+        throw error;
+    } finally {
+        await session.close();
+    }
+}
